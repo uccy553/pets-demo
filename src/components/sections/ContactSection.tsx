563578694524dd4a90df1d38702deba995432021
uccy553 <umonday98@gmail.com>
2025-12-27 14:32:56 +0100

    update info

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -294,14 +294,14 @@ export function ContactSection() {
                 >
                     <div className="rounded-2xl overflow-hidden shadow-lg h-80 sm:h-96">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105781.07455721546!2d-118.42631208359374!3d34.05262810000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c7f2af55e307%3A0xb0558079d8c08ced!2sSasha%20pet%20salon!5e0!3m2!1sen!2sng!4v1766392094801!5m2!1sen!2sng"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54205698.59098652!2d169.98082070000004!3d33.9808891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b7d9a65b9bbf%3A0xc2ad765c0a0f1322!2sBrother&#39;s%20Pet%20Grooming!5e0!3m2!1sen!2sng!4v1766841997638!5m2!1sen!2sng"
                             width="100%"
                             height="100%"
                             style={{ border: 0 }}
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
-                            title="Sasha Pet Salon Location"
+                            title="Brother's Pet Grooming Location"
                         />
                     </div>
                 </motion.div>
