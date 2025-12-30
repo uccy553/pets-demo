47e78d3e08804024f654004323e0aaab1280fa69
uccy553 <umonday98@gmail.com>
2025-12-30 01:59:41 +0100

    update info andy paws

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -294,14 +294,14 @@ export function ContactSection() {
                 >
                     <div className="rounded-2xl overflow-hidden shadow-lg h-80 sm:h-96">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54205698.59098652!2d169.98082070000004!3d33.9808891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b7d9a65b9bbf%3A0xc2ad765c0a0f1322!2sBrother&#39;s%20Pet%20Grooming!5e0!3m2!1sen!2sng!4v1766841997638!5m2!1sen!2sng"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.828564645535!2d-87.72592612415873!3d41.78890817099414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e313ea4b1a337%3A0xb14107572f9de532!2sAndys%20Paws%20LLC!5e0!3m2!1sen!2sng!4v1767056000096!5m2!1sen!2sng"
                             width="100%"
                             height="100%"
                             style={{ border: 0 }}
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
-                            title="Brother's Pet Grooming Location"
+                            title="Andys Paws LLC Location"
                         />
                     </div>
                 </motion.div>
