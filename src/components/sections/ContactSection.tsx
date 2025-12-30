68626c5dfcb234851373ddf4b8fc82fc686eef18
uccy553 <umonday98@gmail.com>
2025-12-30 17:04:08 +0100

    update info nane paws

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -294,14 +294,14 @@ export function ContactSection() {
                 >
                     <div className="rounded-2xl overflow-hidden shadow-lg h-80 sm:h-96">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3302.5952057129425!2d-118.35430392450206!3d34.13111141356212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2be5228b986a3%3A0xfcffc44fe4db3f5!2sPaw-llywood%20Pet%20Spa!5e0!3m2!1sen!2sng!4v1767105192327!5m2!1sen!2sng"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3521.173191598598!2d-82.52209732472619!3d28.04973601030746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2c13b461847dd%3A0xba057e065ad9d0db!2sNane%20Paws%20and%20Tails!5e0!3m2!1sen!2sng!4v1767110435926!5m2!1sen!2sng"
                             width="100%"
                             height="100%"
                             style={{ border: 0 }}
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
-                            title="Paw-llywood Pet Spa Location"
+                            title="Nane Paws and Tails Location"
                         />
                     </div>
                 </motion.div>
